511896fee622b7811da8decfab591be602e6cab1
Robert Leisner <robert.leisner@bodoconsult.de>
2020-11-22 20:48:27 +0100

    first commit

---
+++ Bodoconsult.Core.Database.SqlClient.Test/TestsSqlClientConnManager.cs
@@ -0,0 +1,324 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Bodoconsult.Core.Database.SqlClient.Test.Helpers;
+using NUnit.Framework;
+
+namespace Bodoconsult.Core.Database.SqlClient.Test
+{
+    [TestFixture]
+    public class TestsSqlClientConnManager
+    {
+
+        private readonly string _connectionString = TestHelper.SqlServerString;
+
+
+        private IConnManager _db;
+
+        [SetUp]
+        public void Setup()
+        {
+            _db = SqlClientConnManager.GetConnManager(_connectionString);
+
+        }
+
+        /// <summary>
+        /// Test the connection
+        /// </summary>
+        [Test]
+        public void TestTestConnection()
+        {
+
+            var erg = _db.TestConnection();
+
+
+            Assert.IsTrue(erg);
+        }
+
+
+        /// <summary>
+        /// Get a datatable from the database from a plain SQL string (avoid this option due to SQL injection)
+        /// </summary>
+        [Test]
+        public void TestGetDataTableFromSql()
+        {
+
+            const string sql = "SELECT * FROM dbo.Customer";
+
+            var erg = _db.GetDataTable(sql);
+
+            Assert.IsTrue(erg.Rows.Count > 0);
+        }
+
+
+
+        /// <summary>
+        /// Get a datatable from the database from a (parameterized) SqlCommand object (choose this option to avoid SQL injection)
+        /// </summary>
+        [Test]
+        public void TestGetDataTableFromCommand()
+        {
+
+            const string sql = "SELECT * FROM dbo.Customer";
+
+
+            var cmd = new SqlCommand
+            {
+                CommandText = sql
+            };
+
+            // Add parameters here if required
+
+            var erg = _db.GetDataTable(cmd);
+
+
+            Assert.IsTrue(erg.Rows.Count > 0);
+        }
+
+
+        /// <summary>
+        /// Get a datatable from the database from a (parameterized) SqlCommand object (choose this option to avoid SQL injection)
+        /// </summary>
+        [Test]
+        public void TestGetDataTableFromCommandWidthGetCommandWithParameter()
+        {
+            const string sql = "SELECT * FROM dbo.Customer WHERE CustomerId=@ID";
+
+            var cmd = _db.GetCommand();
+            cmd.CommandText = sql;
+
+            var p = _db.GetParameter(cmd, "@ID", GeneralDbType.Int);
+            p.Value = 1;
+
+            // Add parameters here if required
+            var erg = _db.GetDataTable(cmd);
+
+
+            Assert.IsTrue(erg.Rows.Count > 0);
+            Assert.IsTrue(erg.Rows.Count == 1);
+        }
+
+        /// <summary>
+        /// Get a datatable from the database from a (parameterized) SqlCommand object (choose this option to avoid SQL injection)
+        /// </summary>
+        [Test]
+        public void TestGetDataTableFromCommandWidthGetCommand()
+        {
+            const string sql = "SELECT * FROM dbo.Customer";
+
+            var cmd = _db.GetCommand();
+            cmd.CommandText = sql;
+
+            // Add parameters here if required
+            var erg = _db.GetDataTable(cmd);
+
+
+            Assert.IsTrue(erg.Rows.Count > 0);
+        }
+
+
+        /// <summary>
+        /// Get a datatable from the database from a plain SQL string (avoid this option due to SQL injection)
+        /// </summary>
+        [Test]
+        public void TestGetDataReaderFromSql()
+        {
+
+            const string sql = "SELECT * FROM dbo.Customer";
+
+            var erg = _db.GetDataReader(sql);
+
+            Assert.IsTrue(erg.FieldCount > 0);
+        }
+
+
+
+        /// <summary>
+        /// Get a datatable from the database from a (parameterized) SqlCommand object (choose this option to avoid SQL injection)
+        /// </summary>
+        [Test]
+        public void TestGetDataReaderFromCommand()
+        {
+
+            const string sql = "SELECT * FROM dbo.Customer";
+
+
+            var cmd = new SqlCommand
+            {
+                CommandText = sql
+            };
+
+            // Add parameters here if required
+
+            var erg = _db.GetDataReader(cmd);
+
+            Assert.IsTrue(erg.FieldCount > 0);
+        }
+
+
+        /// <summary>
+        /// Get a datatable from the database from a (parameterized) SqlCommand object (choose this option to avoid SQL injection)
+        /// </summary>
+        [Test]
+        public void TestGetDataReaderFromCommandWidthGetCommand()
+        {
+            const string sql = "SELECT * FROM dbo.Customer";
+
+            var cmd = _db.GetCommand();
+            cmd.CommandText = sql;
+
+            // Add parameters here if required
+            var erg = _db.GetDataReader(cmd);
+
+            Assert.IsTrue(erg.FieldCount > 0);
+
+        }
+
+
+        /// <summary>
+        /// Get a datatable from the database from a (parameterized) SqlCommand object (choose this option to avoid SQL injection)
+        /// </summary>
+        [Test]
+        public void TestGetDataReaderFromCommandWidthGetCommandAndParameter()
+        {
+            const string sql = "SELECT * FROM dbo.Customer WHERE [CustomerId]=@ID;";
+
+            var cmd = _db.GetCommand();
+            cmd.CommandText = sql;
+
+            var p = _db.GetParameter(cmd, "@ID", GeneralDbType.Int);
+            p.Value = 1;
+
+            // Add parameters here if required
+            var erg = _db.GetDataReader(cmd);
+
+            Assert.IsTrue(erg.FieldCount > 0);
+            Assert.IsTrue(erg.HasRows);
+
+        }
+
+        /// <summary>
+        /// Execute a plain SQL string (avoid this option due to SQL injection)
+        /// </summary>
+        [Test]
+        public void TestExecFromSql()
+        {
+
+            const string sql = "DELETE FROM dbo.Customer WHERE CustomerId=-99";
+
+            Assert.DoesNotThrow(() => _db.Exec(sql));
+        }
+
+
+        /// <summary>
+        /// Execute a SqlCommand object (choose this option to avoid SQL injection)
+        /// </summary>
+        [Test]
+        public void TestExecFromCommand()
+        {
+
+            const string sql = "DELETE FROM dbo.Customer WHERE CustomerId=@Key";
+
+            // Create command
+            var cmd = new SqlCommand
+            {
+                CommandText = sql
+            };
+
+            // Add a parameter to the command
+            var para = cmd.Parameters.Add("@Key", SqlDbType.Int);
+            para.Value = -99;
+
+            Assert.DoesNotThrow(() => _db.Exec(cmd));
+        }
+
+
+        /// <summary>
+        /// Get a scalar value from database from a plain SQL string (avoid this option due to SQL injection)
+        /// </summary>
+        [Test]
+        public void TestExecWithResultFromSql()
+        {
+
+            const string sql = "SELECT CustomerId FROM dbo.Customer WHERE CustomerId=1";
+
+            var result = _db.ExecWithResult(sql);
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(string.IsNullOrEmpty(result));
+        }
+
+
+        /// <summary>
+        /// Get a scalar value from database from SqlCommand object (choose this option to avoid SQL injection)
+        /// </summary>
+        [Test]
+        public void TestExecWithResultFromCommand()
+        {
+
+            const string sql = "SELECT CustomerId FROM dbo.Customer WHERE CustomerId=@Key";
+
+            // Create command
+            var cmd = new SqlCommand
+            {
+                CommandText = sql
+            };
+
+            // Add a parameter to the command
+            var para = cmd.Parameters.Add("@Key", SqlDbType.Int);
+            para.Value = 1;
+
+            var result = _db.ExecWithResult(cmd);
+
+            Assert.IsNotNull(result);
+            Assert.IsFalse(string.IsNullOrEmpty(result));
+        }
+
+
+        [Test]
+        public void TestExecMultiple()
+        {
+            const string sql = "DELETE FROM Customer WHERE CustomerId=-99";
+
+            var commands = new List<DbCommand>();
+
+            var cmd = new SqlCommand(sql);
+            commands.Add(cmd);
+
+            cmd = new SqlCommand(sql);
+            commands.Add(cmd);
+
+            cmd = new SqlCommand(sql);
+            commands.Add(cmd);
+
+            var result = _db.ExecMultiple(commands);
+
+            Assert.IsTrue(result == 0);
+        }
+
+
+        [TestCase(GeneralDbType.BigInt, SqlDbType.BigInt)]
+        [TestCase(GeneralDbType.Int, SqlDbType.Int)]
+        [TestCase(GeneralDbType.DateTime, SqlDbType.DateTime)]
+        [TestCase(GeneralDbType.DateTime2, SqlDbType.DateTime2)]
+        [TestCase(GeneralDbType.SmallInt, SqlDbType.SmallInt)]
+        [TestCase(GeneralDbType.Binary, SqlDbType.Binary)]
+        [TestCase(GeneralDbType.Char, SqlDbType.Char)]
+        [TestCase(GeneralDbType.Bit, SqlDbType.Bit)]
+        [TestCase(GeneralDbType.Decimal, SqlDbType.Decimal)]
+        [TestCase(GeneralDbType.Float, SqlDbType.Float)]
+        public void Test(GeneralDbType inpuType, SqlDbType expectedType)
+        {
+            // Arrange
+
+            // Act
+            var result = SqlClientConnManager.MapGeneralDbTypeToSqlDbType(inpuType);
+
+            // Assert
+            Assert.AreEqual(expectedType, result);
+        }
+
+
+    }
+}
---
+++ Bodoconsult.Core.Database.SqlClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bodoconsult.Core.Database.SqlClient")]
+[assembly: AssemblyDescription("Simple .NET database layer for Microsoft(r) SqlServer(r) or LocalDb based on System.Data intended for mainly read-only data access i.e. for reporting purposes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bodoconsult EDV-Dienstleistungen GmbH")]
+[assembly: AssemblyProduct("Bodoconsult.Core.DatabaseSqlClient")]
+[assembly: AssemblyCopyright("Copyright © Bodoconsult EDV-Dienstleistungen GmbH 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("237be621-37e1-40ac-9160-14ff6fbf971c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
